0561c911ec8a12f0dea7502dd5775bdcd61257cd
meldrumn <nashmeldrum@gmail.com>
2023-03-01 22:57:08 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission09_meldrumn.Models;
+using Mission09_meldrumn.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission09_meldrumn.Controllers
+{
+    public class HomeController : Controller
+    {
+        //private BookstoreContext context { get; set; }
+
+        // Lambda function shortcut to do what is above, works if you dont need to pass stuff (both no longer needed since done in ef repository)
+        //public HomeController(BookstoreContext temp) => context = temp;
+
+        private IBookstoreRepository repo;
+
+        public HomeController (IBookstoreRepository temp)
+        {
+            repo = temp;
+        }
+
+        public IActionResult Index(int pageNum = 1)
+        {
+            int pageSize = 10;
+
+            var randomname = new BooksViewModel
+            {
+                Books = repo.Books
+                .OrderBy(p => p.BookId)
+                .Skip((pageNum - 1) * pageSize)
+                .Take(pageSize),
+
+                PageInfo = new PageInfo
+                {
+                    TotalNumBooks = repo.Books.Count(),
+                    BooksPerPage = pageSize,
+                    CurrentPage = pageNum
+                }
+            };
+
+            return View(randomname);
+        }
+    }
+}
---
+++ Models/EFBookstoreRespository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission09_meldrumn.Models
+{
+    public class EFBookstoreRespository : IBookstoreRepository
+    {
+        private BookstoreContext context { get; set; }
+        public EFBookstoreRespository (BookstoreContext temp)
+        {
+            context = temp;
+        }
+        public IQueryable<Book> Books => context.Books;
+    }
+}
